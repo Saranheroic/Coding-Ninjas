3ad65a8c98383e2577217a1505fab66f999b1f10
saranheroic <sarann2302@gmail.com>
2024-10-18 17:22:15 +0530

    update 4

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const date= new Date();
-console.log(date)
+// datePicker.js
+
+// Initialize Flatpickr on the input with id 'date'
+document.addEventListener('DOMContentLoaded', function () {
+    flatpickr("#date", {
+        dateFormat: "d-m-Y" // Set the desired format
+    });
+});
